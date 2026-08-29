347bd873fcd4589d7873939d196f1c85c957095e
Mishi Jha <mishijha22@gmail.com>
2026-08-30 01:50:11 +0530

    feat: add vector field to Node, base-case insert for empty graph

--- core/graph.py
+++ core/graph.py
@@ -1,5 +1,6 @@
 import random
 import math
+from core.node import Node
 def assign_layer(M:int)->int:
     mL=1/math.log(M)
     r=random.random()
@@ -7,3 +8,20 @@ def assign_layer(M:int)->int:
         r=random.random()
     layer=math.floor(-math.log(r)*mL)
     return layer
+
+class HNSWGraph:
+    def __init__(self, M=16):
+        self.nodes={}
+        self.entry_point=None
+        self.M=M
+        self.max_layer=-1
+
+        def insert(self,id,vector):
+            layer=assign_layer(self.M)
+            node=Node(id=id,max_layer=layer,vector=vector)
+            if len(self.nodes)==0:
+                self.entry_point=node
+                self.max_layer=layer
+            self.nodes[id]=node
+
+
--- core/node.py
+++ core/node.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
-
+import numpy as np
 @dataclass
 class Node:
     id:int
     max_layer:int
+    vector: np.ndarray
     neighbors:dict=field(default_factory=dict)
